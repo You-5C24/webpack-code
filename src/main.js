e8fa2bbd765d00971f1ff6f81016bd19f334a246
you_5c24 <you_5c24@163.com>
2022-07-23 19:51:21 +0800

    feat: less scss stylus loader处理

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import count from "./js/count.js";
 import sum from "./js/sum.js";
-import "./css/index.css"
+import "./css/index.css";
+import "./less/index.less";
+import "./scss/index.scss";
+import "./stylus/index.styl";
 
 console.log(count(2, 1));
 console.log(sum(1, 2, 3, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,27 @@ module.exports = {
         test: /\.css$/,
         use: [
           "style-loader", // 动态创建 style 标签，把 js 中 css 添加到 html 文件中，使其生效
-          "css-loader" // 把 css 资源编译成 cjs 模块 放到 js 中
+          "css-loader", // 把 css 资源编译成 cjs 模块 放到 js 中
         ],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"],
       },
     ],
   },
