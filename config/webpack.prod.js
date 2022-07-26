6c5dc2ab5f65b833d86481de227f3261c45e4ee6
5C24 <you_5c24@163.com>
2022-07-26 16:22:33 +0800

    feat: css 压缩

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const ESLintWebpackPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 // 获取处理样式的Loaders
 const getStyleLoaders = (preProcessor) => {
@@ -95,6 +96,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "static/css/main.css",
     }),
+    // css压缩
+    new CssMinimizerPlugin(),
   ],
   // 开发服务器
   devServer: {
