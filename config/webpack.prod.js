809fe15d515901f88561e35c01b4c6b446af8202
5C24 <you_5c24@163.com>
2022-07-30 16:05:00 +0800

    feat: 优化

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -34,8 +34,9 @@ module.exports = {
   // output
   output: {
     path: path.resolve(__dirname, "../dist"),
-    filename: "static/js/main.js",
-    // assetModuleFilename: "images/[hash][ext][query]",
+    filename: "static/js/[name].js", // 入口文件打包输出资源命名方式
+    chunkFilename: "static/js/[name].chunk.js", // 动态导入输出资源命名方式
+    assetModuleFilename: "static/media/[name].[hash][ext]", // 图片、字体等资源命名方式（注意用hash）// 入口文件打包输出文件名
     // 自动清空上次打包结果
     clean: true,
   },
@@ -69,16 +70,16 @@ module.exports = {
                 maxSize: 10 * 1024, // 小于10kb的图片会被base64处理
               },
             },
-            generator: {
-              filename: "static/images/[hash:10][ext][query]",
-            },
+            // generator: {
+            //   filename: "static/images/[hash:10][ext][query]",
+            // },
           },
           {
             test: /\.(ttf|woff2?|map4|map3|avi)$/,
             type: "asset/resource",
-            generator: {
-              filename: "static/media/[hash:10][ext][query]",
-            },
+            // generator: {
+            //   filename: "static/media/[hash:10][ext][query]",
+            // },
           },
           {
             test: /\.m?js$/,
@@ -126,6 +127,7 @@ module.exports = {
     // 提取 css 成单独文件
     new MiniCssExtractPlugin({
       filename: "static/css/main.css",
+      chunkFilename: "static/css/[name].chunk.css",
     }),
     // css压缩
     // new CssMinimizerPlugin(),
@@ -168,6 +170,11 @@ module.exports = {
         },
       }),
     ],
+    // 代码分割配置
+    splitChunks: {
+      chunks: "all",
+      // 其他都用默认值
+    },
   },
   // 开发服务器
   devServer: {
